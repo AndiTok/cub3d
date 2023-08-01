bfe325f4dcc1d45fe39d533aca14866081042f9d
atok <ndtok@hotmail.com>
2023-08-01 23:35:12 +0800

    task:ft_checkmap WIP still troubleshooting

--- ft_checkmap.c
+++ ft_checkmap.c
@@ -6,7 +6,7 @@
 /*   By: atok <atok@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 16:23:01 by atok              #+#    #+#             */
-/*   Updated: 2023/08/01 18:02:23 by atok             ###   ########.fr       */
+/*   Updated: 2023/08/01 23:09:41 by atok             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,22 @@ void check_char(t_game *game)
 {
 	int i;
 	int j;
-
+	//each line has null BUT NO "\n"
 	i = 0;
-	while(game->map.xymap[i][j] != 0x00)
+	while(game->map.xymap[i] != 0x00)
 	{
-		// printf("%s",game->map.xymap[i]);
 		j = 0;
 		while(game->map.xymap[i][j] != 0x00)
 		{
-			if(game->map.xymap[i][j] == '0' || game->map.xymap[i][j] == '1')
-				j++;
-			else if(game->map.xymap[i][j] == 'N' || game->map.xymap[i][j] == 'S' ||
-				game->map.xymap[i][j] == 'E' || game->map.xymap[i][j] == 'W')
-				j++;
-			else 
-				ft_exit_error("ERROR\n map\n");
+			write(1,&game->map.xymap[i][j],1);
+			//printf("%c",game->map.xymap[i][j]);
+			// if(game->map.xymap[i][j] == '0' || game->map.xymap[i][j] == '1')
+			// 	j++;
+			// else if(game->map.xymap[i][j] == 'N' || game->map.xymap[i][j] == 'S' ||
+			// 	game->map.xymap[i][j] == 'E' || game->map.xymap[i][j] == 'W')
+			// 	j++;
+			// else 
+			// 	ft_exit_error("ERROR\n map\n");
 			j++;
 		}
 		i++;
@@ -39,7 +40,7 @@ void check_char(t_game *game)
 // 13 is last
 void ft_check_map(t_game *game)
 {
-	int i = 0;
+/* 	int i = 0;
 	int j = 0;
 	while(game->map.xymap[i] != 0x00)
 	{
@@ -48,8 +49,7 @@ void ft_check_map(t_game *game)
 		//printf("%c",game->map.xymap[i][j]);
 		i++;
 	}
-	printf("\n");
+	printf("\n"); */
 	// write(1, "\n", 1);
-
-	// check_char(game);
+	check_char(game);
 }
